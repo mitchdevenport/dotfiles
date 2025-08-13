02f2cd34ef6ed544dc2607478ebc4b3b06fead7d
Mitch Devenport <88671039+mitchdevenport@users.noreply.github.com>
2025-08-13 09:35:20 -0400

    Update setup.sh to remove redundant sourcing
    Removed sourcing of .bashrc_dotfiles in setup script.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ DOTFILES_ROOT=$(exec 2>/dev/null;cd -- $(dirname "$0"); unset PWD; /usr/bin/pwd 
 # .bashrc setup
 cp $DOTFILES_ROOT/.bashrc_dotfiles $HOME/.bashrc_dotfiles
 echo "source $HOME/.bashrc_dotfiles" >> "$HOME/.bashrc"
-source "$HOME/.bashrc"
 
 # Git Config Setup
 git config --global push.autoSetupRemote true
