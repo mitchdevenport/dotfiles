a95efecd1bb2886d6d91802fa0f2b226f51e26a6
Mitch Devenport <88671039+mitchdevenport@users.noreply.github.com>
2025-08-13 12:33:08 -0400

    Update setup.sh for Git config and remove bashrc setup
    Removed .bashrc setup and added Git config for auto setup.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 # #!/usr/bin/env bash
 
-DOTFILES_ROOT=$(exec 2>/dev/null;cd -- $(dirname "$0"); unset PWD; /usr/bin/pwd || /bin/pwd || pwd)
-
-# .bashrc setup
-cp $DOTFILES_ROOT/.bashrc_dotfiles $HOME/.bashrc_dotfiles
-echo "source $HOME/.bashrc_dotfiles" >> "$HOME/.bashrc"
-
-# Git Config Setup
 git config --global push.autoSetupRemote true
