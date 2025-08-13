d6ec54a4f12f94dcd27722e82daf2b99ebe97979
Mitch Devenport <88671039+mitchdevenport@users.noreply.github.com>
2025-08-13 08:55:04 -0400

    Remove script aliases from setup.sh
    Removed several alias definitions for scripts.

--- setup.sh
+++ setup.sh
@@ -49,8 +49,3 @@
 
 # Git Config Setup
 git config --global push.autoSetupRemote true
-
-alias run-dotcom='/workspace/github/script/server'
-alias run-capi='/workspace/github/script/setup-codespaces-copilot-api && /workspace/github/bin/enable-copilot-chat --gh-mcp'
-alias init-sweagentd='/workspace/github/bin/setup-codespaces-copilot-swe-agent && /workspaces/sweagentd/script/docker-self-hosted-runner'
-alias run-sweagentd='/workspaces/sweagentd/script/server'
